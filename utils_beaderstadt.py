b0d96d1c543d6602e976e2de4c9dd4f2d065d616
Alissa Beaderstadt <alissabeaderstadt@outlook.com>
2025-01-17 21:28:52 -0600

    Finalized

--- utils_beaderstadt.py
+++ utils_beaderstadt.py
@@ -2,15 +2,18 @@
 Module: utils_beaderstadt
 
 Purpose: Reusable Module for My Analytics Projects
+Date Created: January 2025
+Course: Data Analytics Fundamentals
 
-Description: This module provides a byline for my analytics projects. 
+Description: This module provides a byline for my analytics projects.
 When we work hard to write useful code, we want it to be reusable.
-A good byline could be used in every Python analytics project we do.
+This module provides a template illustrating the use of important functions 
+such as lists, statistics, and a multiline f-strings.
+
 
 Author: Alissa Beaderstadt
 
-TODO: Change the module name in this opening docstring
-TODO: Change the author in this opening docstring
+
 """
 
 #####################################
@@ -27,25 +30,20 @@ import statistics  # provides mean(), stdev() and more....
 #####################################
 
 # declare a boolean variable (has a value True or False)
-# TODO: Add another or replace this with your own boolean variable
-has_international_clients: bool = True
 is_pet_friendly: bool = True
+
 # declare an integer variable 
-# TODO: Add or replace this with your own integer variable
-years_in_operation: int = 10
 number_of_deliveries: int = 75
+
 # declare a floating point variable
-# TODO: Add or replace this with your own floating point variable
-average_client_satisfaction: float = 4.7
 average_daily_sales: float = 3.9
+
 # declare a list of strings
-# TODO: Add or replace this with your own list  
-skills_offered: list = ["Data Analysis", "Machine Learning", "Business Intelligence"]
 tools_used: list = ["SQL", "GitHub", "Python"]
+
 # declare a list of numbers so we can illustrate statistics skills
-# TODO: Add or replace this with your own numeric list  
-client_satisfaction_scores: list = [4.8, 4.6, 4.9, 5.0, 4.7]
 recent_scores_on_skills_drills: list = [5.5, 2.2, 6.6, 9.0]
+
 # Calculate basic statistics using built-in Python functions and the statistics module
 # Recent Scores on Skills Drills Statistics
 min_score: float = min(recent_scores_on_skills_drills)  
@@ -54,8 +52,6 @@ mean_score: float = statistics.mean(recent_scores_on_skills_drills)
 stdev_score: float = statistics.stdev(recent_scores_on_skills_drills)
 
 # Use a Python formatted string (f-string) to show information
-# TODO: Modify the text in the byline to fit your information
-# TODO: Modify the variables in the byline to use your variable names
 byline: str = f"""
 ---------------------------------------------------------
 Stellar Analytics: Delivering Professional Insights
