c69c0c4f7f8ae74637c3e7348d02797979a302f2
Alissa Beaderstadt <alissabeaderstadt@outlook.com>
2025-01-17 19:43:07 -0600

    Update global variables

--- utils_beaderstadt.py
+++ utils_beaderstadt.py
@@ -29,23 +29,23 @@ import statistics  # provides mean(), stdev() and more....
 # declare a boolean variable (has a value True or False)
 # TODO: Add another or replace this with your own boolean variable
 has_international_clients: bool = True
-
+is_pet_friendly: bool = True
 # declare an integer variable 
 # TODO: Add or replace this with your own integer variable
 years_in_operation: int = 10
-
+number_of_deliveries: int = 75
 # declare a floating point variable
 # TODO: Add or replace this with your own floating point variable
 average_client_satisfaction: float = 4.7
-
+average_daily_sales: float = 3.9
 # declare a list of strings
 # TODO: Add or replace this with your own list  
 skills_offered: list = ["Data Analysis", "Machine Learning", "Business Intelligence"]
-
+tools_used: list = ["SQL", "GitHub", "Python"]
 # declare a list of numbers so we can illustrate statistics skills
 # TODO: Add or replace this with your own numeric list  
 client_satisfaction_scores: list = [4.8, 4.6, 4.9, 5.0, 4.7]
-
+recent_scores_on_skills_drills: list = [5.5, 2.2, 6.6, 9.0]
 # Calculate basic statistics using built-in Python functions and the statistics module
 # TODO: Replace these variable names with the variable name of your own numeric list
 min_score: float = min(client_satisfaction_scores)  
